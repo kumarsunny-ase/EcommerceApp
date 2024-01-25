3f2b84eacb8df2034fb03f467ae36a6cab065a7d
Kumar Sunny <kumarsunny1348@gmail.com>
2024-01-26 00:09:19 +0100

    Add navbar in UI

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  constructor(private http: HttpClient) {}
+  products!: IProduct[];
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7256/api/products')
+    .subscribe({
+     next: (response: any) => {
+      this.products = response;
+     }
+    });
+  }
 }
